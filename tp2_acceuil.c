3a58db5379b76cb56c3fc0e4bb1c9712c9c1b0f3
Samuel Pilon <pilon.samuel@hotmail.com>
2016-03-16 13:54:42 -0400

    Modularisation avec tp2_accueil.c et .h fonctionnel

--- tp2_acceuil.c
+++ tp2_acceuil.c
@@ -6,4 +6,74 @@
 
 #include "tp2_acceuil.h"
 
+// Initialisation
+bool initialize(struct Application *application) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+  application->gWindow = SDL_CreateWindow(WINDOW_TITLE,
+      SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (application->gWindow == NULL) {
+    printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+  application->gScreenSurface = SDL_GetWindowSurface(application->gWindow);
+  SDL_FillRect(application->gScreenSurface, NULL,
+      SDL_MapRGB(application->gScreenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(application->gWindow);
+  return true;
+}
 
+// Chargement des ressources
+bool loadMedia(struct Application *application) {
+  application->gImage = loadImage(application);
+  if(application->gImage == NULL){
+    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_MENU, SDL_GetError());
+    return false;
+  }
+  return true;
+}
+
+SDL_Surface* loadImage(struct Application *application){
+  SDL_Surface* newSurface = NULL;
+  SDL_Surface* loadedSurface = IMG_Load(IMAGE_MENU);
+  if(loadedSurface == NULL){
+    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_MENU, "Probleme...");
+  } else {
+    newSurface = SDL_ConvertSurface(loadedSurface, application->gScreenSurface->format, 0);
+    if(newSurface == NULL){
+      printf("Unable to optimize image %s! SDL Error: %s\n", IMAGE_MENU, SDL_GetError());
+    }
+    SDL_FreeSurface(loadedSurface);
+  }
+  return newSurface;
+}
+
+// Boucle de jeu
+void gameLoop(struct Application *application) {
+  bool isRunning = true;
+  while (isRunning) {
+
+    // Boucle de lecture d'événement
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        isRunning = false;
+      }
+    }
+    SDL_BlitSurface(application->gImage, NULL, application->gScreenSurface, NULL);
+    SDL_UpdateWindowSurface(application->gWindow);
+
+    // Délais de 16ms pour avoir environ 60 fps
+    SDL_Delay(16);
+  }
+}
+
+// Liberation des ressources et de SDL
+void shutDown(struct Application *application) {
+  SDL_DestroyWindow(application->gWindow);
+  application->gWindow = NULL;
+  SDL_Quit();
+}
--- tp2_acceuil.h
+++ tp2_acceuil.h
@@ -4,4 +4,77 @@
  *
 */
 #include "sdl2.h"
+#include <stdbool.h>
 
+/**
+ * Image du menu principal
+ */
+#define IMAGE_MENU "assets/imagesAccueil/PlayEasy.png"
+
+/**
+ * Title de la fenêtre principale.
+ */
+#define WINDOW_TITLE "Bob le mineur"
+/**
+ * Largeur de la fenêtre principale.
+ */
+#define SCREEN_WIDTH 640
+/**
+ * Hauteur de la fenêtre principale.
+ */
+#define SCREEN_HEIGHT 480
+
+
+
+/**
+ * La structure Application contient toutes les informations requises par la SDL.
+ */
+struct Application {
+  /**
+   * La fenêtre utilisée par le jeu.
+   */
+  SDL_Window* gWindow;
+  /**
+   * La surface de rendu.
+   */
+  SDL_Surface* gScreenSurface;
+  /**
+   * L'image chargée sur la surface.
+   */
+  SDL_Surface* gImage;
+};
+
+/**
+ * Initialise la SDL et remplis {@link #Application application}.
+ * @param application Un pointeur vers la structure Application à remplir.
+ * @return True si l'initialisation a réussi.
+ */
+bool initialize(struct Application *application);
+
+/**
+ * Effectue la boucle de jeu en traitant les messages reçu par la SDL.
+ * La boucle de jeu s'effectue environ 60 fois par seconde pour un rendu de 60 fps.
+ * @param application Un pointeur vers la structure Application à utiliser.
+ */
+void gameLoop(struct Application *application);
+
+/**
+ * Ferme la SDL correctement en libérant les ressources nécessaires.
+ * @param application Un pointeur vers la structure Application à libérer.
+ * @return True si la libération des ressources a réussi.
+ */
+void shutDown(struct Application *application);
+
+/**
+ * Charge les ressources graphiques en mémoire.
+ * @param application Un pointeur vers la structure Application à utiliser.
+ * @return True si le chargement a réussi.
+ */
+bool loadMedia(struct Application *application);
+
+/**
+ * Charge une image dans un SDL_Surface.
+ * @params application Un pointeur vers la structure Application à utiliser.
+ * @return SDL_Surface contenant l'image ou NULL.
+ */
+SDL_Surface* loadImage(struct Application *application);

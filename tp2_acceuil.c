ae268195a2f6ab943d729ee8e1febaa29cc7679f
Samuel Pilon <pilon.samuel@hotmail.com>
2016-03-16 19:25:28 -0400

    (ft. Giulia) Ajout des methodes loopMenu et define des images du menu (non fonctionnel, mais compile)

--- tp2_acceuil.c
+++ tp2_acceuil.c
@@ -10,7 +10,7 @@
 bool loadMedia(struct Application *application) {
   application->gImage = loadImage(application);
   if(application->gImage == NULL){
-    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_MENU, SDL_GetError());
+    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_PE, SDL_GetError());
     return false;
   }
   return true;
@@ -18,15 +18,39 @@ bool loadMedia(struct Application *application) {
 
 SDL_Surface* loadImage(struct Application *application){
   SDL_Surface* newSurface = NULL;
-  SDL_Surface* loadedSurface = IMG_Load(IMAGE_MENU);
+  SDL_Surface* loadedSurface = IMG_Load(IMAGE_PE);
   if(loadedSurface == NULL){
-    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_MENU, "Probleme...");
+    printf("Unable to load image %s! SDL Error : %s\n", IMAGE_PE, "Probleme...");
   } else {
     newSurface = SDL_ConvertSurface(loadedSurface, application->gScreenSurface->format, 0);
     if(newSurface == NULL){
-      printf("Unable to optimize image %s! SDL Error: %s\n", IMAGE_MENU, SDL_GetError());
+      printf("Unable to optimize image %s! SDL Error: %s\n", IMAGE_PE, SDL_GetError());
     }
     SDL_FreeSurface(loadedSurface);
   }
   return newSurface;
+}
+
+bool loopMenu(struct Application *application){
+	bool isRunning = true;
+  while (isRunning) {
+    // Boucle de lecture d'événement
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      switch(e.type){
+      	case SDL_KEYDOWN:
+      		switch(e.key.keysym.sym){
+      			case SDLK_UP:
+      			case SDLK_DOWN:
+      			case SDLK_RIGHT:
+      			case SDLK_LEFT:
+      			case SDLK_RETURN:
+      			default:
+      				break;
+      		}
+      }
+    }
+    SDL_BlitSurface(application->gImage, NULL, application->gScreenSurface, NULL);
+    SDL_UpdateWindowSurface(application->gWindow);
+  }
 }
--- tp2_acceuil.h
+++ tp2_acceuil.h
@@ -10,7 +10,23 @@
 /**
  * Image du menu principal
  */
-#define IMAGE_MENU "assets/imagesAccueil/PlayEasy.png"
+#define IMAGE_PE "assets/imagesAccueil/PlayEasy.png"
+#define IMAGE_PM "assets/imagesAccueil/PlayMedium.png"
+#define IMAGE_PH "assets/imagesAccueil/PlayHard.png"
+#define IMAGE_DE "assets/imagesAccueil/DiffEasy.png"
+#define IMAGE_DM "assets/imagesAccueil/DiffMedium.png"
+#define IMAGE_DH "assets/imagesAccueil/DiffHard.png"
+#define IMAGE_QE "assets/imagesAccueil/QuitEasy.png"
+#define IMAGE_QM "assets/imagesAccueil/QuitMedium.png"
+#define IMAGE_QH "assets/imagesAccueil/QuitHard.png"
+
+enum State{PLAY, DIFFICULTY, QUIT};
+enum Difficulty{EASY, MEDIUM, HARD};
+
+struct Menu{
+	enum State stateMenu;
+	enum Difficulty diffMenu;
+};
 
 /**
  * Charge les ressources graphiques en mémoire.
@@ -25,3 +41,10 @@ bool loadMedia(struct Application *application);
  * @return SDL_Surface contenant l'image ou NULL.
  */
 SDL_Surface* loadImage(struct Application *application);
+
+/**
+ * 
+ * @params application Un pointeur vers la structure Application à utiliser.
+ * @return True si on commence le jeu, False si on quitte.
+ */
+bool loopMenu(struct Application *application);

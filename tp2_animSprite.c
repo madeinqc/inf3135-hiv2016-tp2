d166f4b71c644ed606cbc810abe0de1dad26e424
Samuel Pilon <pilon.samuel@hotmail.com>
2016-03-24 13:20:54 -0400

    Legere modification

--- tp2_animSprite.c
+++ tp2_animSprite.c
@@ -21,20 +21,18 @@ Sprite *CreateSprite(const char* filename, int numRows, int numColumns, int numF
 	if (surface == NULL) {
 		printf("Unable to load image %s! SDL_image Error: %s\n",filename, IMG_GetError());
 	} else {
-        newSprite->texture = SDL_CreateTextureFromSurface(renderer, surface);
+      newSprite->texture = SDL_CreateTextureFromSurface(renderer, surface);
 		if (newSprite->texture == NULL) {
 			printf("Unable to create texture from %s! SDL Error: %s\n", filename, SDL_GetError());
     }
-        newSprite->spriteWidth = surface->w / numColumns;
-        newSprite->spriteHeight = surface->h / numRows;
+      newSprite->spriteWidth = surface->w / numColumns;
+      newSprite->spriteHeight = surface->h / numRows;
 	}
-
-
 	return newSprite;
 }
 
 void DeleteSprite(Sprite *sprite){
-
+	free(sprite);
 }
 
 void RenderSprite(Sprite *sprite, int x, int y){

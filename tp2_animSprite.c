8cbbb3408a3d56b659a148ea1aae64c460ee8bc5
Samuel Pilon <pilon.samuel@hotmail.com>
2016-03-26 13:42:27 -0400

    Format de base de anim_sprite

--- tp2_animSprite.c
+++ tp2_animSprite.c
@@ -3,7 +3,6 @@
  * Description a venir... 
  *
 */
-
 #include "tp2_animSprite.h"
 
 Sprite *CreateSprite(const char* filename, int numRows, int numColumns, int numFrames, 
@@ -19,22 +18,41 @@ Sprite *CreateSprite(const char* filename, int numRows, int numColumns, int numF
 	newSprite->texture = NULL;
 	SDL_Surface *surface = IMG_Load(filename);
 	if (surface == NULL) {
-		printf("Unable to load image %s! SDL_image Error: %s\n",filename, IMG_GetError());
+	  printf("Unable to load image %s! SDL_image Error: %s\n",filename, IMG_GetError());
 	} else {
       newSprite->texture = SDL_CreateTextureFromSurface(app->renderer, surface);
 		if (newSprite->texture == NULL) {
-			printf("Unable to create texture from %s! SDL Error: %s\n", filename, SDL_GetError());
+		  printf("Unable to create texture from %s! SDL Error: %s\n", filename, SDL_GetError());
     }
-      newSprite->spriteWidth = surface->w / numColumns;
-      newSprite->spriteHeight = surface->h / numRows;
+    newSprite->spriteWidth = surface->w / numColumns;
+    newSprite->spriteHeight = surface->h / numRows;
 	}
+	app->renderer = SDL_CreateRenderer(app->gWindow, -1, SDL_RENDERER_ACCELERATED);
+  if (app->renderer == NULL){
+    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+    return false;
+  }
+  SDL_SetRenderDrawColor(app->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	return newSprite;
 }
 
 void DeleteSprite(Sprite *sprite){
-	free(sprite);
+  free(sprite);
 }
 
 void RenderSprite(Sprite *sprite, int x, int y){
-
+	if (sprite->lastUpdate == -1){
+		sprite->lastUpdate == SDL_GetTicks();
+	}
+	int elapsed = SDL_GetTicks() - sprite->lastUpdate;
+	if (elapsed > sprite->delayBetweenFrame){
+		int f = elapsed / sprite->delayBetweenFrame;
+		sprite->currentFrame = (sprite->currentFrame + f) & sprite->nbFrames;
+		sprite->lastUpdate += elapsed;
+	}
+	int srcx = sprite->spriteWidth * (sprite->currentFrame % sprite->nbColumns);
+	int srcy = sprite->spriteHeight * (sprite->nbFrames / sprite->nbColumns);
+	SDL_Rect srcrect = {srcx, srcy, sprite->spriteWidth, sprite->spriteHeight};
+	SDL_Rect dstrect = {x, y, sprite->spriteWidth, sprite->spriteHeight};
+	SDL_RenderCopy(sprite->renderer, sprite->texture, &srcrect, &dstrect);
 }
--- tp2_animSprite.h
+++ tp2_animSprite.h
@@ -9,11 +9,12 @@
 
 #include <stdbool.h>
 #include "sdl2.h"
+#include "tp2_image.h"
+#include "tp2_window.h"
 #include "tp2_application.h"
 #include "tp2_scene.h"
-#include "tp2_window.h"
 #include "tp2_sound.h"
-#include "tp2_image.h"
+
 
 
 /*

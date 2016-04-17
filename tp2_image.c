bf391379b1b053f8a35a6994cf50af14b445ca8b
Samuel Pilon <pilon.samuel@hotmail.com>
2016-04-17 14:03:08 -0400

    Documentation de image.c et .h faite

--- tp2_image.c
+++ tp2_image.c
@@ -1,7 +1,7 @@
 #include "tp2_image.h"
 
-SDL_Texture* tp2image_load(struct Application *application, char *imagePath){
-  SDL_Texture* loadedSurface = IMG_LoadTexture(application->gRenderer, imagePath);
+SDL_Texture* tp2image_load(struct Application *app, char *imagePath){
+  SDL_Texture* loadedSurface = IMG_LoadTexture(app->gRenderer, imagePath);
   if(loadedSurface == NULL){
     printf("Unable to load image %s! SDL Error : %s\n", imagePath, "Probleme...");
   }
--- tp2_image.h
+++ tp2_image.h
@@ -6,9 +6,10 @@
 
 /**
  * Charge une image dans un SDL_Texture.
- * @params application Un pointeur vers la structure Application à utiliser.
+ * @param app Un pointeur vers la structure Application à utiliser.
+ * @param	imagePath Le chemin vers l'image à charger.
  * @return SDL_Surface contenant l'image ou NULL.
  */
-SDL_Texture* tp2image_load(struct Application *application, char *imagePath);
+SDL_Texture* tp2image_load(struct Application *app, char *imagePath);
 
 #endif
